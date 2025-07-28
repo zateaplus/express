41811821a39b3dcf9af25b2d630ce181ba37dcae
zateaplus <zateaplus@gmail.com>
2025-07-28 16:23:57 +0900

    add env

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var yesnoRouter = require('./routes/yesno');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 app.use('/yesno', yesnoRouter);
+app.use('/notes_from_b',notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cat.js
+++ routes/cat.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const cors = require('cors');//corsミドルウェアを追加
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
 request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
